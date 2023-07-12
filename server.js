648d4d231c6a2ef2b3f1973ec994e1fa67eca029
PXTN45 <120036577+PXTN45@users.noreply.github.com>
2023-07-12 11:38:21 +0700

    api

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+const sql = require("./models/db");
+const PORT = 5000;
+
+//creat server
+const app = express();
+
+//user middelwere
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
+app.get("/", (req,res)=> {
+    res.send("<h1> this is a restaurant API </h1>");
+});
+
+app.listen(PORT,() =>{
+    console.log("server is runing on http://localhost:"+ PORT);
+})
